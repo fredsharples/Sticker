5f6b541d1f4d306d7e86d765934bfef95d5caf38
Fred Sharples <fredsharples@gmail.com>
2024-09-24 07:52:12 -0700

    Initial Commit

---
+++ Sticker/StickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickerApp.swift
+//  Sticker
+//
+//  Created by Fred Sharples on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct StickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
