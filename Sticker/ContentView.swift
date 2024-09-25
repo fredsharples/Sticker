6425eb0a0881a8125f9b3a77852ae4d2ff7ca779
Fred Sharples <fredsharples@gmail.com>
2024-09-25 08:29:48 -0700

    initial commit. This only has a grid view of images

--- Sticker/ContentView.swift
+++ Sticker/ContentView.swift
@@ -1,24 +1,38 @@
-//
-//  ContentView.swift
-//  Sticker
-//
-//  Created by Fred Sharples on 9/24/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+    let imageCount = 12 // Total number of images to display
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(1...imageCount, id: \.self) { index in
+                    Image(uiImage: loadImage(index: index))
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 100)
+                        .background(Color.gray.opacity(0.3))
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    func loadImage(index: Int) -> UIImage {
+        let imageName = String(format: "image_%04d", index)
+        if let image = UIImage(named: imageName) {
+            return image
+        } else {
+            print("Failed to load image: \(imageName)")
+            return UIImage(systemName: "photo") ?? UIImage()
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
